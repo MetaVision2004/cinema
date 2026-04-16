6cb99bf42210d02dbf50b37a162617a38184ff45
MetaVision2004 <serjegomare@gmail.com>
2026-04-15 20:37:00 -0500

    refactor: move configuration to environment variables and rename requirements file

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
-DB_HOST = '127.0.0.1'
-DB_PORT = 3306
-DB_USER = 'root'
-DB_PASSWORD = ''
-DB_NAME = 'cinema'
+import os
 
-MAIL_SERVER = 'smtp.gmail.com'
-MAIL_PORT = 587
-MAIL_USE_TLS = True
-MAIL_USERNAME = 'dev.daviar@gmail.com'
-MAIL_PASSWORD = 'xkljhmsavdvbuded'
-EMAIL_DEFAULT_SENDER = 'dev.daviar@gmail.com'
+DB_HOST = os.environ.get('DB_HOST', 'railway')
+DB_PORT = int(os.environ.get('DB_PORT', 3306))
+DB_USER = os.environ.get('DB_USER', 'root')
+DB_PASSWORD = os.environ.get('DB_PASSWORD', '')
+DB_NAME = os.environ.get('DB_NAME', 'cinema')
+
+MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
+MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
+MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'True').lower() in ['true', '1', 't']
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME', 'dev.daviar@gmail.com')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', 'xkljhmsavdvbuded')
+EMAIL_DEFAULT_SENDER = os.environ.get('EMAIL_DEFAULT_SENDER', 'dev.daviar@gmail.com')
